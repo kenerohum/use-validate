9db125cb78428fe5582153fefc98bfff82fcbd67
kejota <kejota9@gmail.com>
2024-11-09 01:22:26 -0300

    ajustes na estrutura do projeto 3

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve("src", 'context/index.tsx'),
+      name: 'use-validate',
+      fileName: (format) => `use-validate.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React'
+        }
+      }
+    }
+  },
   plugins: [react()],
 })
